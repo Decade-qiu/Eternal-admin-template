9ad7c11d723093a1e738d29207f6319960a23f5a
Decade-qiu <3237550600@qq.com>
2024-01-31 11:07:28 +0800

    :sparkles: home view develop complete

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,58 @@
 <template>
-    <div></div>
+    <div>
+        <el-card>
+            <div class="box">
+                <img :src="info.avatar" alt="" class="avatar" />
+                <div class="bottom">
+                    <h3 class="title">{{ getTime() }}好呀，{{ info.name }}！</h3>
+                    <p class="subtitle">硅谷甄选运营平台</p>
+                </div>
+            </div>
+        </el-card>
+        <div class="bottoms">
+            <svg-icon name="welcome" width="800px" height="400px"></svg-icon>
+        </div>
+    </div>
 </template>
-
-<script setup lang='ts'>
-import { onMounted } from 'vue'
-import { ElNotification } from 'element-plus';
-import useUserStore from '@/store/modules/user'
-const userStore = useUserStore();
-onMounted(() => {
-    userStore.userInfo()
-        .then((res) => {
-            // console.log(res.value.avatar);
-        })
-        .catch((error) => {
-            ElNotification({
-                type: 'error',
-                message: error.message,
-            });
-        });
-});
+  
+<script setup lang="ts">
+import { getTime } from '@/utils/time';
+//引入用户相关的仓库,获取当前用户的头像、昵称
+import useUserStore from '@/store/modules/user';
+//获取存储用户信息的仓库对象
+let userStore = useUserStore();
+let info = userStore.data;
 </script>
+  
+<style lang="scss" scoped>
+.box {
+    display: flex;
 
-<style scoped lang="scss"></style>
+    .avatar {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+    }
+
+    .bottom {
+        margin-left: 20px;
+
+        .title {
+            font-size: 30px;
+            font-weight: 900;
+            margin-bottom: 30px;
+        }
+
+        .subtitle {
+            font-style: italic;
+            color: skyblue;
+        }
+    }
+}
+
+.bottoms {
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+}
+</style>
